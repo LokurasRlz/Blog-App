8772e08387bfcfbf89647e0b69684f9554324366
Matias Aguirre <aguirremati09@gmail.com>
2022-11-25 17:14:47 -0300

    Validation and models tests

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,9 +1,10 @@
 class Like < ApplicationRecord
-  belongs_to :user, classname: 'User', foreign_key: 'user_id'
-  belongs_to :post, classname: 'Post', foreign_key: 'post_id'
+  belongs_to :user
+  belongs_to :post
+
   after_save :update_likes_counter
 
   def update_likes_counter
-    post.increment!(:likes_count)
+    post.increment!(:likes_counter)
   end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,10 +1,16 @@
 class Post < ApplicationRecord
-  belongs_to :user, classname: 'User', foreign_key: :user_id
-  has_many :comments, foreign_key: :post_id
-  has_many :likes, foreign_key: :post_id
+  belongs_to :user
+  has_many :comments
+  has_many :likes
+
+  after_save :update_posts_counter
+
+  validates :title, presence: true, length: { maximum: 250 }
+  validates :comments_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :likes_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
   def update_posts_counter
-    user.increment!(:post_counter)
+    user.increment!(:posts_counter)
   end
 
   def recent_five_comments
