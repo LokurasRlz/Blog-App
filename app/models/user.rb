1f8034adcba2dc5004085e856bf25c42a7348905
Matias Aguirre <aguirremati09@gmail.com>
2022-11-25 13:35:42 -0300

    Fixed Linter Errors

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,10 @@
 class User < ApplicationRecord
-    has_many :posts, foreign_key: :user_id
-    has_many :comments, through: :posts, foreign_key: :user_id
-    has_many :likes, through: :posts, foreign_key: :user_id
-  
-    # Compare this snippet from app\models\post.rb:
-    def recent_three_posts
-      posts.limit(3).order(created_at: :desc)
-    end
+  has_many :posts, foreign_key: :user_id
+  has_many :comments, through: :posts, foreign_key: :user_id
+  has_many :likes, through: :posts, foreign_key: :user_id
+
+  # Compare this snippet from app\models\post.rb:
+  def recent_three_posts
+    posts.limit(3).order(created_at: :desc)
   end
+end
