fe567ba950b3c879348b77cf330664934ac2db49
Matias Aguirre <aguirremati09@gmail.com>
2022-12-03 16:53:55 -0300

    add new endpoint and routs for comment controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,18 +1,20 @@
 class CommentsController < ApplicationController
-  def create
-    @post = Post.find(params[:post_id])
-    @user = User.find(params[:user_id])
+  def new
+    comment = Comment.new
     respond_to do |format|
-      format.html do
-        values = params.require(:comment).permit(:text)
-        @new_comment = Comment.new(post: @post, author: @user, text: values[:text])
-        if @new_comment.save
-          flash.now[:success] = 'Comment was saved'
-        else
-          flash.now[:error] = 'Failed to add comment'
-        end
-        redirect_to user_post_url(id: params[:post_id])
-      end
+      format.html { render :new, locals: { comment: } }
+    end
+  end
+
+  def create
+    user = User.find(params[:user_id])
+    post = Post.find(params[:post_id])
+    comment = Comment.new(text: params[:comment][:text], post:, author: current_user)
+    if comment.save
+      redirect_to user_post_path(user, post)
+    else
+      flash.now[:error] = 'Error: comment could not be saved'
+      render :new, locals: { comment: }
     end
   end
 end
