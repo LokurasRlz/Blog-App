48f7ee3310b4259440f236e2cd003329cd6d6223
addis <betsega23@gmail.com>
2022-12-06 18:06:42 +0300

    Update users spec file

---
+++ spec/views/users_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+RSpec.describe "Users index", type: :view do
+    before(:example) do
+        @user = User.create!(name: 'Bob', photo: 'https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png', bio: 'I am Bob')
+        @post = Post.create!(title: 'My first post', text: 'This is my first post', user_id: @user.id)
+    end
+
+    it "displays the user's information" do
+        visit users_path
+        expect(page).to have_content(@user.name)
+        expect(page).to have_content(@user.bio)
+        expect(page).to have_content(@user.photo)
+        expect(page).to have_content(@post.length)
+    end
+end
+
+
