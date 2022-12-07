5b00dc33756f7de62a72f0885928aeef575edb7a
Matias Aguirre <aguirremati09@gmail.com>
2022-12-07 13:04:24 -0300

    fixed user error

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,16 +2,18 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable, :confirmable
   has_many :posts, foreign_key: 'author_id'
   has_many :likes, foreign_key: 'author_id'
   has_many :comments, foreign_key: 'author_id'
+  validates :name, presence: true
+  validates :posts_counter, numericality: { only_integer: true }, comparison: { greater_than_or_equal_to: 0 }
 
-  after_create do
-    update(posts_counter: 0)
+  before_create do
+    posts_counter = 0
   end
 
   def recent_posts
-    posts.order(created_at: :desc).limit(3)
+    posts.includes(:author, :likes, :comments).order(created_at: :desc).limit(3)
   end
 end
