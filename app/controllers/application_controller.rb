4d2c30399c4a1d75268593e145a4f5e01bf671a4
Matias Aguirre <aguirremati09@gmail.com>
2022-12-08 21:40:01 -0300

    added API endpoints

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,20 +1,30 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery prepend: true
-  before_action :update_allowed_parameters, if: :devise_controller?
-  before_action :authenticate_user!
+  include Pagy::Backend
+  include Response
+  include ExceptionHandler
 
-  rescue_from CanCan::AccessDenied do |exception|
-    redirect_to root_url, alert: exception.message
+  protect_from_forgery with: :exception, unless: -> { request.format.json? }
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  def authorize_request
+    header = request.headers['Authorization']
+    header = header.split.last if header
+    begin
+      @decoded = JsonWebToken.decode(header)
+      @user_id = params[:user_id]
+    rescue ActiveRecord::RecordNotFound => e
+      render json: { errors: e.message }, status: :unauthorized
+    rescue JWT::DecodeError => e
+      render json: { errors: "JWT_ error: #{e.message}" }, status: :unauthorized
+    else
+      render json: { errors: 'Missing token' }, status: :unauthorized unless @decoded
+    end
   end
 
   protected
 
-  def update_allowed_parameters
-    devise_parameter_sanitizer.permit(:sign_up) do |u|
-      u.permit(:name, :email, :password, :password_confirmation)
-    end
-    devise_parameter_sanitizer.permit(:account_update) do |u|
-      u.permit(:name, :bio, :email, :password, :current_password)
-    end
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i[name email password password_confirmation])
+    devise_parameter_sanitizer.permit(:account_update, keys: %i[name email password current_password])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,21 @@
 Rails.application.routes.draw do
-  devise_for :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  # Defines the root path route ("/")
-  root 'users#index'
-  resources :users, only: %i[index show] do
-    resources :posts, only: %i[index show new create destroy] do
-      resources :comments, only: %i[create destroy]
-      resources :likes, only: %i[create]
+  devise_for :users, sign_out_via: [:get, :post]
+
+  namespace :api, defaults: { format: :json } do
+    post 'auth/login', to: 'authentication#login'
+    resources :users, only: %i[create update destroy] do
+      resources :posts, only: [:index, :show] do
+        resources :comments, only: [:index, :create]
+    end
+   end
+  end
+  resources :users, only: [:index, :show] do
+    resources :posts, only: [:index, :show, :create, :new, :destroy] do
+      resources :comments, only: [:create, :new, :destroy]
+        resources :likes, only: [:create]
     end
   end
+    root to: 'users#index'
 end
+
+  
